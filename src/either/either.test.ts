56964c7c9599293c6d9d61423f20b0357f6cf98d
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2024-09-08 17:59:32 +0100

    refactor(tests): Centralize tests for `map`, `flatMap` and `match` into monad and matchable using rol perspective

--- src/either/either.test.ts
+++ src/either/either.test.ts
@@ -30,25 +30,9 @@ describe('Either monad', () => {
 
   it.each([
     { type: 'Right', either: Either.right(2), closure: (x: number) => x, expected: Either.right(2) },
-    { type: 'Left', either: Either.left(2), closure: (x: number) => x * 2, expected: Either.left(2) },
-  ])('$type should handle map operation correctly', ({ either, closure, expected }) => {
-    expect(either.map(closure)).toEqual(expected);
-  });
-
-  it.each([
-    { type: 'Right', either: Either.right(2), closure: (x: number) => x, expected: Either.right(2) },
     { type: 'Left', either: Either.left(2), closure: (x: number) => x * 2, expected: Either.left(4) },
   ])('$type should handle mapLeft operation correctly', ({ either, closure, expected }) => {
     expect(either.mapLeft(closure)).toEqual(expected);
-  });
-
-  it.each([
-    { type: 'Right', either: Either.right(2), closure: (x: number) => Either.right(x), expected: Either.right(2) },
-    { type: 'Left', either: Either.left(2), closure: (x: number) => Either.left(x), expected: Either.left(2) },
-  ])('$type should handle flatMap operation correctly', ({ either, closure, expected }) => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    expect(either.flatMap(closure)).toEqual(expected);
   });
 
   it.each([
@@ -58,18 +42,6 @@ describe('Either monad', () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-expect-error
     expect(either.flatMapLeft(closure)).toEqual(expected);
-  });
-
-  it.each([
-    { type: 'Right', either: Either.right(2), expected: '2' },
-    { type: 'Left', either: Either.left('Error'), expected: 'Error' },
-  ])('$type should handle match operation correctly', ({ either, expected }) => {
-    expect(
-      either.match(
-        (x) => x.toString(),
-        (x) => x
-      )
-    ).toEqual(expected);
   });
 
   it.each([
--- src/option/option.test.ts
+++ src/option/option.test.ts
@@ -37,37 +37,6 @@ describe('Option monad', () => {
   });
 
   it.each([
-    { type: 'Some', option: Option.of(2), closure: (x: number) => x * 2, expected: new Some(4) },
-    { type: 'None', option: Option.of<number>(undefined), closure: (x: number) => x * 2, expected: new None() },
-  ])('$type should handle map operation correctly', ({ option, closure, expected }) => {
-    expect(option.map(closure)).toEqual(expected);
-  });
-
-  it.each([
-    { type: 'Some', option: Option.of(2), closure: (x: number) => Option.of(x * 2), expected: new Some(4) },
-    {
-      type: 'None',
-      option: Option.of<number>(undefined),
-      closure: (x: number) => Option.of(x * 2),
-      expected: new None(),
-    },
-  ])('$type should handle flatMap operation correctly', ({ option, closure, expected }) => {
-    expect(option.flatMap(closure)).toEqual(expected);
-  });
-
-  it.each([
-    { type: 'Some', option: Option.of(2), expected: 2 },
-    { type: 'None', option: Option.of<number>(undefined), expected: 2 },
-  ])('$type should handle match operation correctly', ({ option, expected }) => {
-    expect(
-      option.match(
-        (x) => x,
-        () => 2
-      )
-    ).toEqual(expected);
-  });
-
-  it.each([
     { type: 'Some', option: Option.of(2), expected: false },
     { type: 'None', option: Option.of<number>(undefined), expected: true },
   ])('$type should handle isNone operation correctly', ({ option, expected }) => {

1c864882485841c84f6b20074c449a63f25ad853
Daniel Álvarez <danielalvarezm09@gmail.com>
2024-10-11 13:39:24 +0100

    feat(io): doc io monad
    Co-authored-by: Leto Rodríguez <leto4100@gmail.com>
    Co-authored-by: Borja García <borjagarcia.dev@gmail.com>

--- src/io/io.ts
+++ src/io/io.ts
@@ -1,19 +1,63 @@
 import { Monad } from '../monad';
 
+/**
+ * Class representing an IO monad, which encapsulates a side-effectful computation description.
+ * @template T The type of the value produced by the IO computation.
+ */
 class IO<T> implements Monad<T> {
+  /**
+   * Creates a new `IO` instance.
+   * @param {() => T} description The side-effectful computation.
+   * @private
+   */
   private constructor(private description: () => T) {}
 
+  /**
+   * Creates an `IO` instance from a side-effectful computation.
+   * @template T The type of the value produced by the IO computation.
+   * @param {() => T} sideEffect The side-effectful computation.
+   * @returns {IO<T>} A new `IO` instance.
+   * @example
+   * const io = IO.of(() => console.log('Hello, World!'));
+   * io.runUnsafe(); // Hello, World!
+   */
   static of<T>(sideEffect: () => T): IO<T> {
     return new IO(sideEffect);
   }
 
+  /**
+   * Transforms the value produced by this `IO` instance into another `IO` instance.
+   * @template U The type of the transformed value.
+   * @param {(value: T) => IO<U>} transform The transformation function.
+   * @returns {IO<U>} A new `IO` instance containing the transformed value.
+   * @example
+   * const io = IO.of(() => 5).flatMap(value => IO.of(() => value * 2));
+   * console.log(io.runUnsafe()); // 10
+   */
   flatMap<U>(transform: (value: T) => IO<U>): IO<U> {
     return new IO(() => transform(this.description()).runUnsafe());
   }
+
+  /**
+   * Transforms the value produced by this `IO` instance.
+   * @template U The type of the transformed value.
+   * @param {(value: T) => U} transform The transformation function.
+   * @returns {IO<U>} A new `IO` instance containing the transformed value.
+   * @example
+   * const io = IO.of(() => 5).map(value => value * 2);
+   * console.log(io.runUnsafe()); // 10
+   */
   map<U>(transform: (value: T) => U): IO<U> {
     return new IO(() => transform(this.description()));
   }
 
+  /**
+   * Executes the side-effectful computation and returns the result.
+   * @returns {T} The result of the side-effectful computation.
+   * @example
+   * const io = IO.of(() => 5);
+   * console.log(io.runUnsafe()); // 5
+   */
   runUnsafe(): T {
     return this.description();
   }

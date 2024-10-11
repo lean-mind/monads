71cde4f0b428976611c1223372c9f974fe90586d
Daniel Álvarez <danielalvarezm09@gmail.com>
2024-10-11 16:05:26 +0100

    feat(either): doc either monad
    Co-authored-by: Leto Rodríguez <leto4100@gmail.com>
    Co-authored-by: Borja García <borjagarcia.dev@gmail.com>

--- src/either/either.ts
+++ src/either/either.ts
@@ -1,15 +1,49 @@
 import { Monad } from '../monad';
 import { Matchable } from '../match';
 
+/**
+ * Abstract class representing a value that can be one of two possible types.
+ * @template L The type of the left value.
+ * @template R The type of the right value.
+ */
 abstract class Either<L, R> implements Monad<R>, Matchable<R, L> {
+  /**
+   * Creates a `Right` instance.
+   * @template T The type of the right value.
+   * @param {T} value The right value.
+   * @returns {Either<never, T>} A `Right` instance containing the value.
+   * @example
+   * const right = Either.right(5);
+   * right.match(console.log, error => console.error(error.message)); // 5
+   */
   static right<T>(value: T): Either<never, T> {
     return new Right(value);
   }
 
+  /**
+   * Creates a `Left` instance.
+   * @template T The type of the left value.
+   * @param {T} value The left value.
+   * @returns {Either<T, never>} A `Left` instance containing the value.
+   * @example
+   * const left = Either.left('error');
+   * left.match(console.log, error => console.error(error.message)); // 'error'
+   */
   static left<T>(value: T): Either<T, never> {
     return new Left(value);
   }
 
+  /**
+   * Creates an `Either` instance from a `Matchable` instance.
+   * @template L The type of the left value.
+   * @template R The type of the right value.
+   * @param {Matchable<R, L>} matchable The matchable instance.
+   * @returns {Either<L, R>} A `Right` instance if the matchable contains a right value, otherwise a `Left` instance.
+   * @example
+   * const option = Option.of(5);
+   * const either = Either.from(option);
+   * either.match(console.log, error => console.error(error.message)); // 5
+   */
   static from<L, R>(matchable: Matchable<R, L>): Either<L, R> {
     return matchable.match<Either<L, R>>(
       (value: R) => Either.right(value),
@@ -17,6 +51,15 @@ abstract class Either<L, R> implements Monad<R>, Matchable<R, L> {
     );
   }
 
+  /**
+   * Executes a function and returns an `Either` instance.
+   * @template T The type of the right value.
+   * @param {() => T} execute The function to execute.
+   * @returns {Either<Error, T>} A `Right` instance if the function executes without error, otherwise a `Left` instance.
+   * @example
+   * const result = Either.catch(() => JSON.parse('invalid json'));
+   * result.match(console.log, error => console.error(error.message)); // 'SyntaxError: Unexpected token i in JSON at position 0'
+   */
   static catch<T>(execute: () => T): Either<Error, T> {
     try {
       return Either.right(execute());
@@ -25,22 +68,91 @@ abstract class Either<L, R> implements Monad<R>, Matchable<R, L> {
     }
   }
 
+  /**
+   * Transforms the right value contained in this `Either` instance.
+   * @template T The type of the transformed value.
+   * @param {(r: R) => T} transform The transformation function.
+   * @returns {Either<L, T>} A new `Either` instance containing the transformed value.
+   * @example
+   * const result = Either.right(5).map(value => value * 2);
+   * result.match(console.log, console.error); // 10
+   */
   abstract map<T>(transform: (r: R) => T): Either<L, T>;
 
+  /**
+   * Transforms the left value contained in this `Either` instance.
+   * @template T The type of the transformed value.
+   * @param {(l: L) => T} transform The transformation function.
+   * @returns {Either<T, R>} A new `Either` instance containing the transformed value.
+   * @example
+   * const result = Either.left('error').mapLeft(value => `Error: ${value}`);
+   * result.match(console.log, console.error); // 'Error: error'
+   */
   abstract mapLeft<T>(transform: (l: L) => T): Either<T, R>;
 
+  /**
+   * Transforms the right value contained in this `Either` instance into another `Either` instance.
+   * @template T The type of the transformed value.
+   * @param {(r: R) => Either<L, T>} transform The transformation function.
+   * @returns {Either<L, T>} The result of the transformation function.
+   * @example
+   * const result = Either.right(5).flatMap(value => Either.right(value * 2));
+   * result.match(console.log, console.error); // 10
+   */
   abstract flatMap<T>(transform: (r: R) => Either<L, T>): Either<L, T>;
 
+  /**
+   * Transforms the left value contained in this `Either` instance into another `Either` instance.
+   * @template T The type of the transformed value.
+   * @param {(l: L) => Either<T, R>} transform The transformation function.
+   * @returns {Either<T, R>} The result of the transformation function.
+   * @example
+   * const result = Either.left('error').flatMapLeft(value => Either.left(`Error: ${value}`));
+   * result.match(console.log, console.error); // Error: error
+   */
   abstract flatMapLeft<T>(transform: (l: L) => Either<T, R>): Either<T, R>;
 
+  /**
+   * Matches the right or left value contained in this `Either` instance.
+   * @template T The type of the result.
+   * @param {(r: R) => T} ifRight The function to call if this is a `Right` instance.
+   * @param {(l: L) => T} ifLeft The function to call if this is a `Left` instance.
+   * @returns {T} The result of the matching function.
+   * @example
+   * const result = Either.right(5);
+   * result.match(console.log, console.error); // 5
+   */
   abstract match<T>(ifRight: (r: R) => T, ifLeft: (l: L) => T): T;
 
+  /**
+   * Checks if this is a `Left` instance.
+   * @returns {boolean} `true` if this is a `Left` instance, otherwise `false`.
+   * @example
+   * const result = Either.left('error');
+   * console.log(result.isLeft()); // true
+   */
   abstract isLeft(): this is Left<L, R>;
 
+  /**
+   * Checks if this is a `Right` instance.
+   * @returns {boolean} `true` if this is a `Right` instance, otherwise `false`.
+   * @example
+   * const result = Either.right(5);
+   * console.log(result.isRight()); // true
+   */
   abstract isRight(): this is Right<L, R>;
 }
 
+/**
+ * Class representing a left value.
+ * @template L The type of the left value.
+ * @template R The type of the right value.
+ */
 class Left<L, R> extends Either<L, R> {
+  /**
+   * Creates a new `Left` instance.
+   * @param {L} value The left value.
+   */
   constructor(private value: L) {
     super();
   }
@@ -74,7 +186,16 @@ class Left<L, R> extends Either<L, R> {
   }
 }
 
+/**
+ * Class representing a right value.
+ * @template L The type of the left value.
+ * @template R The type of the right value.
+ */
 class Right<L, R> extends Either<L, R> {
+  /**
+   * Creates a new `Right` instance.
+   * @param {R} value The right value.
+   */
   constructor(private value: R) {
     super();
   }

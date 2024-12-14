79a69bfd55d6e8d1e8b2830b8197292d39006be9
Mario Pinto <mario.pinto.miranda@gmail.com>
2024-12-14 13:04:38 +0000

    refactor(future-monad): remove unnecessary interface

--- src/complete/completable.ts
+++
@@ -1,3 +0,0 @@
-export interface Completable<T> {
-  complete<S>(onSuccess: (value: T) => S, onFailure: (error: Error) => S): Promise<S>;
-}
--- src/complete/index.ts
+++
@@ -1,1 +0,0 @@
-export { Completable } from './completable';
